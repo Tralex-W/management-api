a3ecd482655255e014455f6660f4d09f43c29df2
Tralex-W <eric.wramling@gmail.com>
2026-01-08 15:46:20 +0100

    feature: add formation to represent errors in a single string

---
+++ src/utils/formats.js
@@ -0,0 +1,11 @@
+//represent errors in a single string
+
+export const FORMAT_VALIDATION_ERRORS = (error) => {
+  if (!error || !error.issues) return "Validion failed";
+
+  if (Array.isArray(error.issues)) {
+    return error.issues.map((issue) => issue.message).join(", ");
+  }
+
+  return JSON.stringify(error);
+};
