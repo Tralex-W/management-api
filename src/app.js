7783647f9f1c13faeccb606c87268d39973101a5
Tralex-W <eric.wramling@gmail.com>
2026-01-08 14:51:54 +0100

    feature: added logging with winston

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import LOGGER from "./config/logger.js";
 
 const APP = express();
 
 APP.get("/", (req, res) => {
+  LOGGER.info("TEST LOGGER");
   res.status(200).send("TEST");
 });
 
