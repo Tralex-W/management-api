7eb8ea9c4608be4ec6a883341c5c0b6608624d45
Tralex-W <eric.wramling@gmail.com>
2026-01-09 12:17:46 +0100

    add comment and change message names

--- run-docker.sh
+++ run-docker.sh
@@ -4,5 +4,6 @@
 #PORT=3000
 #NAME=management-api
 #ENV=.env
+#MOUNT logs to /app/logs in container for the log files
 
 docker run -d -p 3000:3000 --env-file .env --mount type=bind,source=./logs,target=/app/logs --name management-api management-api
